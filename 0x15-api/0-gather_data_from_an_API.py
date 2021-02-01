3c7038c1a66e940668f7ef65fc63898860e1cfc9
valen2510 <valenjaramillo1025@hotmail.com>
2021-02-01 18:04:14 +0000

    | 01 | Write a Python script that, using this REST API for a given employee ID. |

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Write a Python script that, using this REST API,
+    for a given employee ID, returns information about
+    his/her TODO list progress.
+"""
+
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    url = 'https://jsonplaceholder.typicode.com/users/'
+    response_user = requests.get(url + argv[1])
+    employee_name = response_user.json()['name']
+
+    response_todos = requests.get(url + argv[1] + '/todos')
+    total_tasks = response_todos.json()
+    done_tasks = []
+    for dic in total_tasks:
+        if dic['completed']:
+            done_tasks.append(dic['title'])
+
+    print('Employee {} is done with tasks({}/{})'
+          .format(employee_name, len(done_tasks), len(total_tasks)))
+    for tasks in done_tasks:
+        print('\t', tasks)
